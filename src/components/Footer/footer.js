1d6640d93d1eb78f13ed49cbcebb527365449912
zaids2641 <39379399+zaids2641@users.noreply.github.com>
2021-06-22 23:56:59 +0800

    Update footer.js

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -7,7 +7,7 @@ const AppFooter = props => {
                 Created by: Felix Diaz III
             </p>
             <p>
-                <a href="#">Full Codes in GitHub</a>
+                <a href="https://github.com/zaids2641/simple-colorpicker" target="_blank" rel="noopener noreferref" >Full Codes in GitHub</a>
             </p>
         </footer>
     )
